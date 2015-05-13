0e1b4e1b11191f111c26c984837f707efdb6ab17
orestisf <orestisf1993@gmail.com>
2015-05-13 20:55:26 +0300

    Adds heurestic rule file and comments in steepest descent

---
+++ work3/rules/heurestic_gamma_rule.m
@@ -0,0 +1,6 @@
+function [gamma] = heurestic_gamma_rule(x, y, g, gamma)
+%heurestic_gamma_rule Heurestic rule for gamma. To be implemented.
+
+
+end
+
--- work3/steepest_descent.m
+++ work3/steepest_descent.m
@@ -1,22 +1,26 @@
 function [k, x, y, point] = steepest_descent(varargin)
-%steepest_descent2 Steepest descent method with variable gamma
+%steepest_descent Steepest descent method with gamma given from variable input.
 
+% default values
 p = inputParser;
 default_e = 1e-7;
 default_c = 2;
 default_point = [0; 0];
 default_gamma = 0.5;
 
+% data type checking functions
 is_point = @(x) (isnumeric(x) && all(size(x) == [2 1]) && all(isfinite(x)));
 is_positive_number = @(x) (isnumeric(x) && all(x > 0) && isscalar(x) && all(isfinite(x)));
 is_function_handle = @(x) (isa(x, 'function_handle'));
 
+% parameters to the parser
 addParameter(p,'c', default_c, is_positive_number);
 addParameter(p,'gamma', default_gamma, is_positive_number);
 addParameter(p,'e', default_e, is_positive_number);
 addParameter(p,'point', default_point, is_point);
 addRequired(p, 'gamma_rule', is_function_handle);
 
+% parse inputs and evaluate values. Initialize variables
 parse(p, varargin{:});
 e = p.Results.e;
 gamma = p.Results.gamma;
@@ -28,6 +32,7 @@ x = point(1, k);
 y = point(2, k);
 g = gradf(x ,y);
 
+% main loop
 while norm(g) >= e
     gamma = gamma_rule(x, y, g, gamma);
     
