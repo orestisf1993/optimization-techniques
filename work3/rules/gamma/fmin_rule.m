99dcf4ffa0fd0d8369100c652fd6b1dd264ce19c
orestisf <orestisf1993@gmail.com>
2015-05-21 15:37:42 +0300

    Better fmin performance

--- work3/rules/gamma/fmin_rule.m
+++ work3/rules/gamma/fmin_rule.m
@@ -3,7 +3,7 @@ function [gamma] = fmin_rule(x, g, ~)
 
 g = g(:, end);
 to_min = @(gamma) f(x(1, end) - gamma * g(1), x(2, end) - gamma * g(2));
-gamma = fminsearch(to_min, 0.5);
+gamma = fminsearch(to_min, 0.5, optimset('TolX', 1e-2));
 
 % gamma = fminbnd(to_min, 0.1, 1.5/norm(g));
 % gamma = fminbnd(to_min, 0.1, 1);
