f8dea7f1f4158c3616e521a01699b6bd9585c09e
orestisf <orestisf1993@gmail.com>
2015-05-21 16:55:09 +0300

    Adds bounded fmin rule

---
+++ work3/rules/gamma/fmin_rule_bounded.m
@@ -0,0 +1,8 @@
+function [gamma] = fmin_rule_bounded(x, g, ~)
+%fmin_rule Line search rule.
+
+g = g(:, end);
+to_min = @(gamma) f(x(1, end) - gamma * g(1), x(2, end) - gamma * g(2));
+gamma = fminbnd(to_min, 0.1, 1);
+
+end
