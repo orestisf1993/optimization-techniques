3c3347e37bdee7856c5870654e3767dc9c80810e
orestisf <orestisf1993@gmail.com>
2015-05-21 15:33:02 +0300

    Small changes in generic algorithm

--- work3/generic_algorithm.m
+++ work3/generic_algorithm.m
@@ -1,11 +1,12 @@
-function [k, x, g] = generic_algorithm(varargin)
-%steepest_descent Steepest descent method with gamma given from variable input.
+function [total_iterations, x, g] = generic_algorithm(varargin)
+%generic_algorithm 
 
 % default values
 p = inputParser;
-default_e = 1e-7;
-default_x = [0; 0];
+default_e = 1e-8;
+default_x = [-2; -1];
 default_gamma = 0.8;
+default_max_k = 1e4;
 
 % data type checking functions
 is_point = @(x) (isnumeric(x) && all(size(x) == [2 1]) && all(isfinite(x)));
@@ -15,6 +16,7 @@ is_function_handle = @(x) (isa(x, 'function_handle'));
 % parameters to the parser
 addParameter(p,'gamma', default_gamma, is_positive_number);
 addParameter(p,'e', default_e, is_positive_number);
+addParameter(p, 'max_k', default_max_k, is_positive_number);
 addParameter(p,'start_x', default_x, is_point);
 addParameter(p, 'use_hessian', false, @islogical);
 addRequired(p, 'd_rule', is_function_handle);
@@ -28,28 +30,22 @@ x = p.Results.start_x;
 gamma_rule = p.Results.gamma_rule;
 d_rule = p.Results.d_rule;
 use_hessian = p.Results.use_hessian;
-
-display(e)
-display(gamma)
-display(x)
-display(gamma_rule)
-display(use_hessian)
+max_k = p.Results.max_k;
 
 k = 1;
+total_iterations = 1;
 g = gradf(x(1,k) ,x(2,k));
 
 gamma_inputs = {x, g, gamma};
 d_inputs = {x, g, NaN};
 
 if use_hessian
-     h = hessianf(x(1,k), x(2,k));
+    h = hessianf(x(1,k), x(2,k));
     d_inputs{4} = h;
 end
 
-
 d = d_rule(d_inputs{:});
 gamma = gamma_rule(gamma_inputs{:});
-total_iterations = 0;
 
 % main loop
 while norm(g(:, k)) >= e
@@ -70,9 +66,8 @@ while norm(g(:, k)) >= e
     d = d_rule(d_inputs{:});
     
     k = k + 1;
-    if (k > 10000) 
+    if (k > max_k) 
         break; 
     end
 end
-display(total_iterations);
 end
